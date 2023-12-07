a9c64f0606fa12817b0a9663c3d01dfd00751484
baeseungwon <21storeo@gmail.com>
2023-12-07 11:28:27 +0900

    msg

---
+++ item-service/app/__init__.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+import os
+
+
+naming_convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+
+db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
+migration = Migrate()
+bcrypt = Bcrypt()
+jwt = JWTManager()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_envvar('APP_CONFIG_FILE')
+
+    CORS(app)
+    db.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+
+    if app.config['SQLALCHEMY_DATABASE_URI'].startswith("sqlite"):
+        migration.init_app(app, db, render_as_batch=True)
+    else:
+        migration.init_app(app, db)
+
+    from app.api import api
+
+    app.register_blueprint(api, url_prefix="/api")
+
+    return app
