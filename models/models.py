3d6a61c1dbfb760a5207fe4d55c5bb6b55294139
Suah-Cho <sacho0929@gmail.com>
2023-12-12 10:04:14 +0900

    feat: base 데이터베이스 세팅

--- models/models.py
+++ models/models.py
@@ -18,24 +18,24 @@ class Stores(db.Model):
     store_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(20), nullable=False)
-    owner_name = db.Column(db.String(20), nullalbe=False)
+    owner_name = db.Column(db.String(20), nullable=False)
     phone = db.Column(db.String(20), nullable=False)
     store_name = db.Column(db.String(30), nullable=False)
     address = db.Column(db.String(100), nullable=False)
     store_type = db.Column(db.String(20), nullable=False)
 
 
-class Categories(db.Modle):
+class Categories(db.Model):
     category_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), nullalbe=False)
-    store_id = db.Column(db.Integer, db.ForeignKey('stores.store_id', nullable=False))
+    name = db.Column(db.String(20), nullable=False)
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.store_id'), nullable=False)
 
 
 class Items(db.Model):
     item_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullalbe=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('categories.category_id', ondelete='CASCADE', nullalbe=False))
-    price = db.Column(db.Integer, nullalbe=False)
+    name = db.Column(db.String(50), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.category_id', ondelete='CASCADE'), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String(200), nullable=True)
 
 
@@ -43,17 +43,17 @@ class Orders(db.Model):
     order_id = db.Column(db.Integer, primary_key=True)
     table_number = db.Column(db.Integer, nullable=False)
     is_processing = db.Column(db.Boolean, nullable=True)
-    cart_id = db.Column(db.Integer, db.ForeignKey('cart.cart_id', ondelete='CASCADE', nullable=False))
+    cart_id = db.Column(db.Integer, db.ForeignKey('cart.cart_id', ondelete='CASCADE'), nullable=False)
 
 
 class Carts(db.Model):
     cart_id = db.Column(db.Integer, primary_key=True)
-    order_id = db.Column(db.Integer, db.ForeignKey('orders.order_id', ondelete='CASCADE', nullable=False))
-    item_id = db.Column(db.Integer, db.ForeignKey('items.item_uid', ondelete='CASCADE', nullable=False))
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.order_id', ondelete='CASCADE'), nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey('items.item_uid', ondelete='CASCADE'), nullable=False)
 
 
 class Sales(db.Model):
     sale_id = db.Column(db.Integer, primary_key=True)
     total_price = db.Column(db.Integer, nullable=False)
-    sale_date = db.Column(db.DateTime(), nullalbe=False)
-    order_id = db.Column(db.Integer, db.ForeignKey('orders.order_id', ondelete='CASCADE', nullable=False))
+    sale_date = db.Column(db.DateTime(), nullable=False)
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.order_id', ondelete='CASCADE'), nullable=False)
