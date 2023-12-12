46f70e8693db8138fe21bdbc77cddb7d41657432
baeseungwon <21storeo@gmail.com>
2023-12-12 11:33:56 +0900

    feat: 스토어 등록 스토어 등록 API 개발

---
+++ store_service/app/api/stores.py
@@ -0,0 +1,76 @@
+from flask import Blueprint, jsonify, request
+from models.models import db
+from models.models import Stores
+from flask_bcrypt import *
+from flask_jwt_extended import *
+from flask_wtf.csrf import CSRFProtect
+
+
+bp = Blueprint('stores', __name__, url_prefix='/stores')
+
+
+@bp.route('/', methods=['POST'])
+def create_store():
+    data = request.get_json()
+
+    username = data['username']
+    password = generate_password_hash(data['password']).decode('utf-8')
+    owner_name = data['owner_name']
+    phone = data['phone']
+    store_name = data['store_name']
+    address = data['address']
+    store_type = data['store_type']
+
+    store = Stores(username=username, password=password, owner_name=owner_name, phone=phone, store_name=store_name,
+                   address=address, store_type=store_type)
+    db.session.add(store)
+    db.session.commit()
+
+    return jsonify(store.serialize), 201
+
+
+@bp.route('/<int:store_id>', methods=['GET'])
+def get_store(store_id):
+    store = Stores.query.get(store_id)
+    return jsonify(store.serialize)
+
+
+@bp.route('/<int:store_id>', methods=['PUT'])
+def update_store(store_id):
+    store = Stores.query.get(store_id)
+    data = request.get_json()
+
+    store.username = data['username']
+    store.password = generate_password_hash(data['password']).decode('utf-8')
+    store.owner_name = data['owner_name']
+    store.phone = data['phone']
+    store.store_name = data['store_name']
+    store.address = data['address']
+    store.store_type = data['store_type']
+
+    db.session.commit()
+
+    return jsonify(store.serialize)
+
+
+@bp.route('/<int:store_id>', methods=['DELETE'])
+def delete_store(store_id):
+    store = Stores.query.get(store_id)
+    db.session.delete(store)
+    db.session.commit()
+
+    return jsonify(store.serialize)
+
+
+@bp.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+
+    store = Stores.query.filter_by(username=username).first()
+
+    if store and check_password_hash(store.password, password):
+        return jsonify(store.serialize)
+    else:
+        return jsonify({'error': 'Invalid username or password'}), 400
