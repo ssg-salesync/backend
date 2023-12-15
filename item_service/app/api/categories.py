ac9b4e5d0a895ad7d807f8cb7d16111eba4bc013
Suah-Cho <sacho0929@gmail.com>
2023-12-15 14:57:30 +0900

    fix: item service 아이템 서비스 categories.py 삭제 api 수정

--- item_service/app/api/categories.py
+++ item_service/app/api/categories.py
@@ -51,7 +51,7 @@ def post_category() :
         "result": "success",
         "message": "카테고리 등록 성공",
         "id": new_category.category_id
-    }, 200
+    }, 201
 
 # 카테고리 수정
 @bp.route('/<int:category_id>', methods=['PUT'])
@@ -75,23 +75,16 @@ def put_category(category_id: int) :
         "result": "success",
         "message": "카테고리 수정 성공",
         "id": category_id
-    }, 200
+    }, 201
 
 
 
 # 카테고리 삭제
 @bp.route('/<int:category_id>', methods=['DELETE'])
 def delete_category(category_id: int) :
-        
-    category = Categories.query.filter_by(category_id=category_id).first()
 
-    if category is None:
-        return {
-            "result": "failed",
-            "message": '카테고리가 존재하지 않습니다.'
-        }, 404
+    db.session.query(Categories).filter_by(category_id=category_id).delete()
 
-    db.session.delete(category)
     db.session.commit()
 
     return {
