8bfba4639681549526e7588d46f67549a1b5a539
Suah-Cho <sacho0929@gmail.com>
2023-12-19 12:32:25 +0900

    fix: cors cors 해결

--- employee/app/__init__.py
+++ employee/app/__init__.py
@@ -15,7 +15,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_envvar('APP_CONFIG_FILE')
 
-    CORS(app)
+    CORS(app, resources={r"/*": {"origins": "*"}})
 
     db.init_app(app)
     bcrypt.init_app(app)
