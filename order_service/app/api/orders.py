4d80b6ead8335557d8ff9d353be2c51d060be6d6
Suah-Cho <sacho0929@gmail.com>
2023-12-21 14:48:30 +0900

    fix: order service 최조 신규 등록할 때 오류 메시지 수정

--- order_service/app/api/orders.py
+++ order_service/app/api/orders.py
@@ -21,6 +21,7 @@ def post_order() :
 
     new_sale = Sales(total_price=req['total_price'], sale_date=datetime.datetime.now())
 
+<<<<<<< Updated upstream
     db.session.add(new_sale)
     db.session.commit()
     
@@ -99,12 +100,92 @@ def delete_order(sale_id: int):
             "result": "failed",
             "message": "주문 수정 실패"
         }, 404
+=======
+    existing_order = Orders.query.filter_by(table_no=table_no, paid=False).all()
+
+    if existing_order:
+        return jsonify({
+            "result": "failed",
+            "message": "신규 주문 등록 실패"
+        }), 404
+>>>>>>> Stashed changes
 
     db.session.query(Sales).filter_by(sale_id=sale_id).delete()
 
     db.session.commit()
 
+<<<<<<< Updated upstream
     return {
+=======
+    return jsonify({
+        "result": "success",
+        "message": "결제 성공",
+        "store_id": f"{store_id}",
+        "table_no": table_no,
+    }), 200
+
+
+@bp.route('/', methods=['PUT'])
+@jwt_required()
+def get_order():
+    store_id = get_jwt_identity()
+    req = request.get_json()
+
+    table_no = req['table_no']
+    carts = req['carts']
+
+    orders = Orders.query.filter_by(table_no=table_no, paid=False).all()
+    print(orders, type(orders))
+
+    if not orders:
+        return jsonify({
+            "result": "failed",
+            "message": "존재하지 않는 주문"
+        }), 404
+
+    for order in orders:
+        old_carts = Carts.query.filter_by(order_id=order.order_id).all()
+        for old_cart in old_carts:
+            db.session.delete(old_cart)
+            db.session.commit()
+        db.session.delete(order)
+        db.session.commit()
+
+    order = Orders(store_id=store_id, table_no=table_no, order_date=datetime.now(), paid=False)
+    db.session.add(order)
+    db.session.commit()
+
+    for cart in carts:
+        item_id = cart['item_id']
+        quantity = cart['quantity']
+
+        cart = Carts(order_id=order.order_id, item_id=item_id, quantity=quantity)
+        db.session.add(cart)
+        db.session.commit()
+
+    return jsonify({
+        "result": "success",
+        "message": "주문 수정 성공",
+        "order_id": order.order_id,
+        "table_no": order.table_no
+    }), 200
+
+
+@bp.route('/<table_no>', methods=['DELETE'])
+@jwt_required()
+def delete_order(table_no: int):
+
+    orders = Orders.query.filter_by(table_no=table_no, paid=False).all()
+
+    order_ids = []
+
+    for order in orders:
+        order_ids.append(order.order_id)
+        db.session.delete(order)
+        db.session.commit()
+
+    return jsonify({
+>>>>>>> Stashed changes
         "result": "success",
         "message": "주문 삭제 성공",
         "sale_id": sale_id
