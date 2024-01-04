5ecbf9e676fafba1a5f016b330bd14fe93a06510
Suah-Cho <sacho0929@gmail.com>
2024-01-04 14:07:24 +0900

    weather

--- order_service/app/api/orders.py
+++ order_service/app/api/orders.py
@@ -3,6 +3,8 @@ import requests
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import *
 from ..models import db, Orders, Carts
+import requests
+import xmltodict
 
 
 bp = Blueprint('orders', __name__, url_prefix='/orders')
@@ -200,6 +202,22 @@ def delete_order(table_no: int):
         "table_no": table_no,
         "orders": order_ids
     }), 200
+
+
+@bp.route('/weather', methods=['GET'])
+def getweather():
+
+    currnet_date = datetime.now()
+
+    date = currnet_date.date().strftime("%Y%m%d")
+    time = currnet_date.time().strftime("%H%M")
+
+    response = requests.get(f'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?ServiceKey=itjde0Gu8qwR%2FBOyEoxWD1L%2Fy6FmbK8E34wGcdGVxNRFNhe4v4NNAeMONUDFyChoKf6ih14CugCbreFwp4fbpg%3D%3D&numOfRows=10&pageNo=1&base_date={date}&base_time={time}&nx=54&ny=124')
+
+    json_date = xmltodict.parse(response.text)
+
+
+    return jsonify(json_date), 200
 
 
 def get_carts_in_order(orders):
