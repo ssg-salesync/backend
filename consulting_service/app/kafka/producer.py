1464f1cdf0cf9de710980218b3b235320802dbd2
Suah-Cho <sacho0929@gmail.com>
2024-01-11 14:39:32 +0900

    feat: kafka models

---
+++ consulting_service/app/kafka/producer.py
@@ -0,0 +1,21 @@
+from confluent_kafka import Producer
+import json
+
+def create_producer():
+    producer = Producer({
+        'bootstrap.servers': 'my-kafka.kafka.svc.cluster.local:9092',
+        'security.protocol': 'SASL_PLAINTEXT',
+        'sasl.mechanism': 'SCRAM-SHA-256',
+        'sasl.username': 'admin',
+        'sasl.password': 'password'
+    })
+
+    return producer
+
+
+producer = create_producer()
+
+
+def send_message(topic, message):
+    producer.produce(topic, json.dumps(message).encode('utf-8'))
+    producer.flush()
