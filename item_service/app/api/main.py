a125fd63e411fdb9010499b34460205f663ca582
baeseungwon <21storeo@gmail.com>
2024-01-11 14:08:29 +0900

    msg

--- item_service/app/api/main.py
+++ item_service/app/api/main.py
@@ -1,12 +1,9 @@
-from flask import Blueprint, request, jsonify
-from models.models import db
-from models.models import Categories
-from flask_jwt_extended import *
+from flask import Blueprint
+
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 
-# 카테고리 조회
 @bp.route('/', methods=['GET'])
 def main():
-    return "Hello World!"
+    return "Health Check"
