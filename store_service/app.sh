0ed181a0fc3eeaa104ce38d3b2bb92a1a0c6906b
baeseungwon <21storeo@gmail.com>
2024-01-16 15:08:52 +0900

    msg

--- store_service/app.sh
+++ store_service/app.sh
@@ -1,29 +1,9 @@
-#!/bin/bash
+#!/bin/sh
+# shellcheck disable=SC2164
+cd /salesync/store_service
 
-rollback_and_init_migration() {
-    flask db downgrade base
-    flask db init
-}
+flask db init
+flask db migrate
+flask db upgrade
 
-migrate() {
-    flask db migrate
-}
-
-run_app() {
-    python3 -m flask run --host=0.0.0.0
-}
-
-main() {
-    # shellcheck disable=SC2164
-    cd /salesync/store_service
-
-    migrate_status=$(flask db migrate 2>&1)
-    if [[ $migrate_status == *"Can't locate revision identified by"* ]]; then
-        rollback_and_init_migration
-        migrate
-    fi
-
-    run_app
-}
-
-main
+python3 -m flask run --host=0.0.0.0
