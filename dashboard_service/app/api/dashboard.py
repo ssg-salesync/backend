e3ab83a8608b25290a0e956eb92fc2c689b5685c
baeseungwon <21storeo@gmail.com>
2024-01-19 12:05:05 +0900

    msg

--- dashboard_service/app/api/dashboard.py
+++ dashboard_service/app/api/dashboard.py
@@ -144,12 +144,11 @@ def get_total_volumes():
             date = start + timedelta(days=i)
             date_str = date.strftime('%Y-%m-%d')
             start_str = start.strftime('%Y-%m-%d')
-            end_str = end.strftime('%Y-%m-%d')
 
             sales_resp = requests.get(f'http://service-sale.default.svc.cluster.local/sales/daily',
                                       params={'store_id': store_id, 'date': date_str}).json()
             order_resp = requests.get(f'http://service-order.default.svc.cluster.local/orders/period',
-                                      params={'store_id': store_id, 'start': start_str, 'end': end_str}).json()
+                                      params={'store_id': store_id, 'start': start_str, 'end': start_str}).json()
             item_resp = requests.get(f'http://service-item.default.svc.cluster.local/categories/items',
                                      headers=headers, params={'store_id': store_id}).json()
 
