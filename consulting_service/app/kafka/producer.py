a195c91a992b4d128dff80e1d5eafdd6b851fbff
Suah-Cho <sacho0929@gmail.com>
2024-01-25 15:38:42 +0900

    fix: kafka

--- consulting_service/app/kafka/producer.py
+++ consulting_service/app/kafka/producer.py
@@ -4,7 +4,7 @@ import json
 
 def create_producer():
     producer = Producer({
-        'bootstrap.servers': 'salesync-kafka-controller-0.salesync-kafka-controller-headless.kafka.svc.cluster.local:9092,salesync-kafka-controller-1.salesync-kafka-controller-headless.kafka.svc.cluster.local:9092,salesync-kafka-controller-2.salesync-kafka-controller-headless.kafka.svc.cluster.local:9092',
+        'bootstrap.servers': 'salesync-kafka-pro-controller-0.salesync-kafka-pro-controller-headless.kafka.svc.cluster.local:9092,salesync-kafka-pro-controller-1.salesync-kafka-pro-controller-headless.kafka.svc.cluster.local:9092,salesync-kafka-pro-controller-2.salesync-kafka-pro-controller-headless.kafka.svc.cluster.local:9092',
         # 'bootstrap.servers': 'localhost:9092',
         'security.protocol': 'PLAINTEXT'
     })
